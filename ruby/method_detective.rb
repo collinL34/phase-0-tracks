0b017901815876b41a3ee384dbeefface76aa395
collinL34 <collinlshelby@gmail.com>
2016-08-29 18:46:58 -0700

    case solved

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,47 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+puts "iNvEsTiGaTiOn".swapcase
+  # => “InVeStIgAtIoN”
+puts 'thiS is AwESome'.swapcase
 
-# "zom".<???>
-# => “zoom”
+puts "zom".insert(1,'o')
+  # => “zoom”
+puts 'col'.insert(2,'l')
 
-# "enhance".<???>
-# => "    enhance    "
+puts "enhance".center(15)
+  # => "    enhance    "
+puts 'string'.center(20)
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+puts "Stop! You’re under arrest!".upcase
+  # "STOP! YOU’RE UNDER ARREST!"
+puts 'im amazing!'.upcase
 
-# "the usual".<???>
-#=> "the usual suspects"
+puts "the usual".gsub!(/$/,' suspects')
+  # "the usual suspects"
+puts 'its really hot in'.gsub!(/$/,' chico')
 
-# " suspects".<???>
-# => "the usual suspects"
+puts " suspects".prepend('the usual')
+  # => "the usual suspects"
+puts ' world'.prepend('hello')
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+puts "The case of the disappearing last letter".chomp('r')
+  # => "The case of the disappearing last lette"
+puts "this is my favorite letter p".chomp('p')
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+puts "The mystery of the missing first letter".slice(1..-1)
+  # => "he mystery of the missing first letter"
+puts 'i finally figured it out yaayy'.slice(0..-1)
 
-# "Elementary,    my   dear        Watson!".<???>
+puts "Elementary,    my   dear        Watson!".squeeze(' ')
 # => "Elementary, my dear Watson!"
+puts 'oh dear       sir how     do     you   do ?'.squeeze(' ')
 
-# "z".<???>
-# => 122 
+puts "z".ord
+  # => 122
 # (What is the significance of the number 122 in relation to the character z?)
+puts 'A'.ord
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+puts "How many times does the letter 'a' appear in this string?".count('a')
+  # => 4
+puts 'how many times do i say hi i really like to say hi and i am really just typing hi for my example hi, one ore time hi'.count('i')
