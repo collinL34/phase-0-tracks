654bc3ecc055f84523cfbf2b108f13620712d6a0
collinL34 <collinlshelby@gmail.com>
2016-08-31 20:15:13 -0700

    vampires update/nested practice

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -20,7 +20,30 @@ runway = {
     seats_available: 50,
     models: 6
   },
-  ratings: [],
+  ratings: {
+      heather: {
+        'swimsuit': nil,
+        'lingerie': nil
+      },
+      megan: {
+        'swimsuit': nil,
+        'lingerie': nil
+        },
+      scarlett: {
+        'swimsuit': nil,
+        'lingerie': nil
+    },
+  },
 }
 
-puts runway[:first_show][:models]['heather']
+p runway[:first_show][:models][:heather]
+
+p runway[:first_show][:show_name].upcase
+p runway[:ratings][:heather][:swimsuit] = 10
+p runway[:ratings][:heather][:lingerie] = 12
+p runway[:ratings][:megan][:swimsuit] = 5
+p runway[:ratings][:megan][:lingerie] = 6
+p runway[:ratings][:scarlett][:swimsuit] = 4
+p runway[:ratings][:scarlett][:lingerie] = 7
+p runway[:ratings]
+p runway[:seating][:seats_available]
--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,16 +1,21 @@
-puts 'How many employees are we processing today?'
+p 'How many employees are we processing today?'
 x = gets.chomp.to_i
+
+vampire_results = nil
+
 
 while x > 0
 
-    puts 'What is your name?'
+    p 'What is your name?'
     name = gets.chomp
 
-    if name == "Drake Cula"
-      puts '“Definitely a vampire.'
-    end
+      if name == 'Drake Cula' || name == 'adam'
+        vampire_results = 'Definitely a vampire'
+        p vampire_results
+        x = x - 1
+      else
 
-    puts 'How old are you?'
+    p 'How old are you?'
     age = gets.chomp.to_i
 
     if age > 200
@@ -19,7 +24,7 @@ while x > 0
       age = true
     end
 
-    puts 'What year were you born?'
+    p 'What year were you born?'
     year = gets.chomp.to_i
 
     if year > 1800
@@ -28,7 +33,7 @@ while x > 0
       year = true;
     end
 
-    puts 'Our company cafeteria serves garlic bread. Should we order some for you?'
+    p 'Our company cafeteria serves garlic bread. Should we order some for you?'
     garlic_bread = gets.chomp
 
     if garlic_bread == 'yes'
@@ -37,7 +42,7 @@ while x > 0
       garlic_bread = false
     end
 
-    puts 'Would you like to enroll in the company’s health insurance?'
+    p 'Would you like to enroll in the company’s health insurance?'
     health = gets.chomp
 
     if health == 'yes'
@@ -49,11 +54,11 @@ while x > 0
     user = true
 
     while user == true
-      puts 'please list your allergies.'
+      p 'please list your allergies.'
         allergies = gets.chomp
 
        if allergies == 'sunshine'
-          puts 'Probably a vampire'
+          vampire_results = 'Probably a vampire'
             user = false
       elsif allergies == 'done'
           user = false
@@ -61,18 +66,21 @@ while x > 0
     end
 
     if name == false
-      puts '“Definitely a vampire.'
+       vampire_results = 'Definitely a vampire.'
     elsif (age == true) && (garlic_bread == true || health == true)
-      puts 'Probably not a vampire.'
+      vampire_results = 'Probably not a vampire.'
     elsif (age == false) && (garlic_bread == false || health == false)
-      puts 'Probably a vampire.'
+      vampire_results = 'Probably a vampire.'
     elsif (age == false) && (garlic_bread == false && health == false)
-      puts 'Almost certainly a vampire.'
+      vampire_results = 'Almost certainly a vampire.'
     else
-      puts 'Results inconclusive'
+      vampire_results = 'Results inconclusive'
     end
 
     x = x - 1
 end
+end
 
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+p vampire_results
+
+p "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
