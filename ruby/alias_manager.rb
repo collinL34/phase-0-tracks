28fdef5cb00152102bdfdb7634b81d1e3599cfb8
collinL34 <collinlshelby@gmail.com>
2016-09-01 21:08:08 -0700

    solo project layout created

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,26 @@
+# make fake spy names like a boss!!
+# reverse the whole string so that first name is now last name
+#but since the words are also now reversed we will re reverse the individual word os it goes back to the correct spelling
+# then loop through the individual words and use next on the array
+# vowels are made the next aeiou a => e
+# and so are constants d => f
+
+
+def secret_name(string) #sam mora
+vowels = ['a','e','i','o','u']
+constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+      word_seperator = string.reverse.chars #aron mas, [a,r,o,n,m,a,s]
+      p word_seperator # [a,r,o,n,m,a,s]
+      new_word = word_seperator.map do |char|
+        if vowels.include?(char) # vowels.index(a)
+          p char.next[vowels.index(char)] #vowels.index(a) => e
+        else
+          p char.next[constants.index(char)]
+        end
+      end
+    p new_word.join(' ')
+    p new_word
+end
+
+p secret_name('sam mora')
+# => arom mas => esun net
