ca50e8c9e9ed0332cd727cbbd67fdb360500a655
collinL34 <collinlshelby@gmail.com>
2016-09-03 14:48:01 -0700

    fib sequence fixed

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -14,28 +14,23 @@ end
 
 p search_array(arr, 89)
 
-# [0,1,1,2,3,5]
-
 #find a way to add the last two numbers of an array together and keep doing that for a number of sequences
 #need to use .next
 #fib(6) [0,1,1,2,3,5] 4.next + 3.next = 2
 #add 0 + 1 then ad again but with a new addition method 0.next + 1
 
-# def fib(num)
-#   counter = 0
-#   while counter <= num
-#     counter + 1
-
-#     counter = counter + 1
-#   end
-# end
-
-
-def fibonacci( n )
-    fib_nums = []
-    return  n  if n <= 1
-    fib_nums << fibonacci( n - 1 )
-    fib_nums << fibonacci( n - 2 )
-    p "#{fib_nums}"
+def fib(n)
+  fib_array  = []
+  a = 0
+  b = 1
+  fib_array.push(a)
+  n.times do
+    temp = a
+    a = b
+    b = temp + b
+    fib_array.push(a)
+  end
+  p fib_array
 end
-puts fibonacci( 10 )
+
+fib(10)
