c3897e0c8dcddb5e4b86c72bac7548e15189f0c7
collinL34 <collinlshelby@gmail.com>
2016-09-04 21:09:23 -0700

    victory!!!!

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,68 +5,33 @@
 # vowels are made the next aeiou a => e
 # and so are constants d => f
 
-
-# def next_constants(char)
-# constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-#   constants.each do |letters|
-#     if letters == char
-#      p idx = constants.index(letters)
-#      char = constants[idx].next
-#     else
-#       p 'shit'
-#     end
-#   end
-#   p char
-# end
-
-
-# def next(char)
-# vowels = ['a','e','i','o','u']
-# constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-#   if (vowels.index(char) != nil)
-#     idx = vowels.index(char)
-#     char = vowels[idx].next
-#   else
-#     idx = constants.index(char)
-#     char = constants[idx].next
-#   end
-#   p char
-# end
-
-# next('a')
-
-# def secret_name(string) #sam mora
-# vowels = ['a','e','i','o','u']
-# constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-#       word_seperator = string.reverse.chars #arom mas, [a,r,o,m,m,a,s]
-#       p word_seperator # [a,r,o,m,m,a,s]
-#       new_word = []
-#       word_seperator.each do |char|
-#         if vowels.include?(char) == true # vowels.index(a)
-#           p new_word << vowels[vowels.index(char)].next #vowels.index(a) => e
-#         else
-#           p new_word << constants[constants.index(char)].next #vowels.index(a) => e
-#         end
-#       end
-#     p new_word.join(' ')
-#     p new_word
-# end
-
-# p secret_name('sam mora')
-# # => arom mas => esun net
-
-
-
 def secret(string)
-  word = string.split('')
-  word.map! do |x|
-      if x != ' '
-        x.next
-      else
-        next
-      end
-  end
+  word = string.reverse.split('')
+  vowels = ['a','e','i','o','u']
+  constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    word.map! do |x|
+        if vowels.include?(x)
+          if x != 'u'
+            idx = vowels.index(x)
+            idx = idx + 1
+            x = vowels[idx]
+          else
+            x = 'a'
+          end
+        elsif constants.include?(x)
+          if x != 'z'
+            idx = constants.index(x)
+            idx = idx + 1
+            x = constants[idx]
+          else
+            x = 'b'
+          end
+        else x == ' '
+          next
+        end
+    end
   p word.join(' ')
 end
 
+secret('collin shulbz')
 secret('heather dean')
