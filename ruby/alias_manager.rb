bbc6d9fc0590a29c532e709740d6aa83467e93df
collinL34 <collinlshelby@gmail.com>
2016-09-04 21:58:25 -0700

    final version

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,33 +5,49 @@
 # vowels are made the next aeiou a => e
 # and so are constants d => f
 
-def secret(string)
-  word = string.reverse.split('')
-  vowels = ['a','e','i','o','u']
-  constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-    word.map! do |x|
-        if vowels.include?(x)
-          if x != 'u'
-            idx = vowels.index(x)
-            idx = idx + 1
-            x = vowels[idx]
-          else
-            x = 'a'
+    loop do
+      puts "Please give me a name, type 'quit' if done."
+      user = gets.chomp
+      if user == 'quit'
+        break
+      end
+      def secret(user)
+        new_versions = []
+        old_versions = []
+        old_versions << string
+        word = string.reverse.split('')
+        vowels = ['a','e','i','o','u']
+        constants = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+          word.map! do |x|
+            if vowels.include?(x)
+              if x != 'u'
+                 idx = vowels.index(x)
+                 idx = idx + 1
+                 x = vowels[idx]
+              else
+                x = 'a'
+              end
+            elsif constants.include?(x)
+              if x != 'z'
+                idx = constants.index(x)
+                idx = idx + 1
+                x = constants[idx]
+              else
+                x = 'b'
+              end
+            elsif x == ' '
+              x = " "
+            end
           end
-        elsif constants.include?(x)
-          if x != 'z'
-            idx = constants.index(x)
-            idx = idx + 1
-            x = constants[idx]
-          else
-            x = 'b'
-          end
-        else x == ' '
-          next
-        end
-    end
-  p word.join(' ')
-end
+            new_versions << word.join('')
 
-secret('collin shulbz')
-secret('heather dean')
+            counter = 0
+
+            while counter < new_versions.length
+              old = old_versions[counter]
+              newer = new_versions[counter]
+                puts "#{old} is also known as #{newer}."
+                counter = counter + 1
+              end
+          end
+      end
