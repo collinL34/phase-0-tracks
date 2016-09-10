a353001bb0abe02f19dd8ac3601cb53da1f68c5b
collinL34 <collinlshelby@gmail.com>
2016-09-10 16:39:36 -0700

    method iteration corrected

--- ruby/hangman/word.rb
+++ ruby/hangman/word.rb
@@ -8,28 +8,28 @@ class Hangman
   attr_accessor :guess
 
   def word(word)
-    @guess_word = word
+    @guess_word = word #'loose'
     @guess = Array.new(word.length) {'-'}.join('')
     @guess
   end
 
-  def finder(char)
-    @character = char
-    guess = ['l','o','o','s','e']
-    counter = 0
-    while counter < guess.length
-      letter = guess[counter]
-      if letter == char
-        result = adder(counter)
+  def finder(char) #o
+    @character = char #o
+    guess = @guess_word.split('')
+    counter = 0 #1
+    while counter < guess.length #5
+      letter = guess[counter] #l #o
+      if letter == char #false #true
+        result = adder(counter) #1 => o
       end
       counter += 1
     end
   p result
 end
 
-  def adder(arg)
-    @guess.insert(arg,@character)
-    @guess.chop!
+  def adder(arg) #1
+    @guess.split('')
+    @guess[arg] = @character
     @guess
   end
 
@@ -38,12 +38,12 @@ end
 game = Hangman.new
 
 puts 'please give me a word'
-user = gets.chomp
+user = gets.chomp #loose
 guess_word = game.word(user)
 word_length = guess_word.length
 
 word_length.times do
   puts "Here is your blank word #{guess_word}, give me a letter"
-  letter = gets.chomp
-  guess_word = game.finder(letter)
+  letter = gets.chomp #o
+  guess_word = game.finder(letter) #o
 end
