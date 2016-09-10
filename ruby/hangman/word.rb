58d0a5d52bfcdacc3cac3df90aa756b6b64e35a9
collinL34 <collinlshelby@gmail.com>
2016-09-09 17:23:49 -0700

    methods modified

--- ruby/hangman/word.rb
+++ ruby/hangman/word.rb
@@ -1,6 +1,5 @@
 #create a class for the hangmanish game
 #decide wether we ask the user/users if their are two players of if they are playing against a computer
-  #if against the computer create a words array that hold the different words the computer could choose from
   #then create an instance of the hangman class for the computer that will have constant variables
   #create a method that changes the output for everytime the user gets a letter correct then adds that letter in the correct index possible array
 
@@ -9,33 +8,10 @@ class Hangman
   attr_reader :guess
   attr_reader :words
 
-  def computer_guess_word
-    computer_words = ["memorandum", "vegetation", "possession", "incredible", "accessible", "chimpanzee", "mastermind", "provincial", "photograph", "confession", "particular", "conscience", "compromise", "gregarious", "litigation", "reluctance", "management", "depression", "instrument", "simplicity"]
-    @computer_word = computer_words[rand(20)]
-    @guess = false
-  end
-
   def guess_word
     guess_words_length = @computer_word.length
     #create a variable that holds the string of emty characters and adds them when guessed
     @words = Array.new(guess_words_length) {'-'}.join('')
-  end
-
-  def guess_word_idx(guess_char)
-    idx = []
-    @Words.scan
-  end
-
-  def correct_guess(char)
-    if @computer_word.include?(char)
-      idx = @computer_word.index(char)
-      @words.insert(idx,char)
-      false
-    else
-      @words
-      false
-      puts 'try again'
-    end
   end
 
 end
