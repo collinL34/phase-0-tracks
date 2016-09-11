6fab87dadd054ad49cde9bd5cb46e0f642598656
collinL34 <collinlshelby@gmail.com>
2016-09-10 17:25:56 -0700

    class completed

--- ruby/hangman/word.rb
+++ ruby/hangman/word.rb
@@ -10,15 +10,15 @@ class Hangman
   def word(word)
     @guess_word = word #'loose'
     @guess = Array.new(word.length) {'-'}.join('')
-    @guess
+    @guess_word
   end
 
   def finder(char) #o
     @character = char #o
-    guess = @guess_word.split('')
+    array_guess = @guess_word.split('')
     counter = 0 #1
-    while counter < guess.length #5
-      letter = guess[counter] #l #o
+    while counter < array_guess.length #5
+      letter = array_guess[counter] #l #o
       if letter == char #false #true
         result = adder(counter) #1 => o
       end
@@ -39,11 +39,34 @@ game = Hangman.new
 
 puts 'please give me a word'
 user = gets.chomp #loose
-guess_word = game.word(user)
-word_length = guess_word.length
+word_to_guess = game.word(user)
+word_length = word_to_guess.length
 
 word_length.times do
-  puts "Here is your blank word #{guess_word}, give me a letter"
+  puts "Here is your blank word #{word_to_guess}, give me a letter"
   letter = gets.chomp #o
-  guess_word = game.finder(letter) #o
+
+  if word_to_guess.include?(letter)
+    guess_word = game.finder(letter) #o
+    if letter == word_to_guess
+      puts 'Congrats you did it!'
+      game.guess = word_to_guess
+      break
+    end
+  elsif letter == 'done'
+    puts 'Quitter'
+    break
+  else
+    puts 'Try again'
+  end
+
+  if game.guess == word_to_guess
+    puts 'Congrats you did it!'
+    break
+  end
+
 end
+
+  if game.guess != word_to_guess
+    puts 'Sorry you lost try again.'
+  end
