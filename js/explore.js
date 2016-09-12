f999345df74646f2f3f82dfae615d69c2fafaa79
collinL34 <collinlshelby@gmail.com>
2016-09-12 13:14:44 -0700

    array variable created to store data

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,4 @@
+// create a var that holds an array to store the reversed strings
 // create a function that take in a string as an arguement and reverses the letters.
  // create a variable that splits the string into char then you must call the method reverse on it and then return th joined variable
  // or create a function that manualy changes the string
@@ -5,17 +6,24 @@
  // then create a for loop that loops over the string that was passed into the method
  // then add the characters from the old string in reverse order into the new var and close the loops and return the new string
 
+var reverseString = [];
+
 function stringReverse(string) {
+  reverseString.push(string)
   return string.split('').reverse().join('')
 }
 
-stringReverse('hello')
+
+reverseString.push(stringReverse('hello'));
 
 function string(strings) {
+  reverseString.push(strings)
   var newStr = '';
   for(var i = strings.length -1; i >= 0;i--) {
     newStr += strings[i];
   };
   return newStr;
 };
-string('hello');
+reverseString.push(string('hello'));
+
+if
