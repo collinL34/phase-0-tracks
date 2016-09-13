1f91dac86065fb08926468d466327c2e62e0c703
collinL34 <collinlshelby@gmail.com>
2016-09-13 16:55:43 -0700

    arrays created

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colorArray = ['blue','green','purple','red'];
+var colors = ['samantha','katie','amrin','collin'];
+
+console.log(colorArray.push('black'));
+console.log(colors.push('heather'));
+
+console.log(colorArray);
