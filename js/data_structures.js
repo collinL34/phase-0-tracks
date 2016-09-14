5210d2d512bf185c984bc3ef5b12939e15ed6b14
collinL34 <collinlshelby@gmail.com>
2016-09-13 17:01:22 -0700

    hash key/value function adder created;

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,15 @@ console.log(colorArray.push('black'));
 console.log(colors.push('heather'));
 
 console.log(colorArray);
+
+var hash = {};
+
+function hashAdder(array1,array2) {
+  for(var i = 0; i < array1.length;i++) {
+    hash[array1[i]] = array2[i];
+  };
+  return hash;
+};
+
+hashAdder(colorArray,colors);
+console.log(hash);
