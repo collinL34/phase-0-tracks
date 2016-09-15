30ed8b5b2e255275faf1a610c5ad9ad25e381525
collinL34 <collinlshelby@gmail.com>
2016-09-14 17:15:10 -0700

    calculator assessment

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -1,21 +1,54 @@
-array = [1,2,3,4,5]
-new_array = []
-i = -1
-array.length.times do
-  new_array << array[i]
-  i-= 1
+# puts 'give me a number.'
+# num = gets.chomp
+
+array = []
+# array << num
+# until num == 'quit'
+#   puts 'give me a num please.'
+#   num = gets.chomp
+#   array << num
+#   if num == 'quit'
+#     p array
+#   end
+# end
+
+
+# begin
+#   puts 'give me a number.'
+#   user = gets.chomp
+#   array << user
+# end until user == 'quit'
+# p array
+
+array = ['heather','collin','sam','conlan']
+hash = {
+  name: 'colin',
+  age: 21,
+  gf: 'heather dean',
+  car: 'crossfire'
+}
+
+# array.each do |letter|
+#   letter[0] = letter[0].upcase
+#   p letter
+# end
+# p array
+
+# array = [1,2,3,4,5]
+# def updaser(array)
+# array.map! do |letter|
+#   letter * 2
+#   letter / 2
+# end
+# p array
+# end
+
+# upcaser(array)
+
+
+
+def calculator(num1,sym,num2)
+  return num1 sym.to_sym num2
 end
 
-puts new_array
-
-i = 0
-
-while i < 10
-  p i
-  i +=1
-end
-begin i > 5
-  p i
-  i += 2
-end while i < 25
-puts i
+calculator()
