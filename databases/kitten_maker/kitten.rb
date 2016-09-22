4a3d5bf377bb11ddc80df8a1194724dbe235c755
collinL34 <collinlshelby@gmail.com>
2016-09-21 20:28:45 -0700

    psuedocode created

--- databases/kitten_maker/kitten.rb
+++ databases/kitten_maker/kitten.rb
@@ -1,2 +1,14 @@
+#steps to creating a ruby sql connection
+
+# link gems
 require 'sqlite3'
 require 'faker'
+
+# create your database
+db = SQLite3::Database.new('kittens.db')
+
+#create table
+
+#create a test
+
+#
