b7d6cff915e861973dcddb99db2ed0377b04a151
collinL34 <collinlshelby@gmail.com>
2016-09-21 21:06:58 -0700

    fixed id problem

--- databases/kitten_maker/kitten.rb
+++ databases/kitten_maker/kitten.rb
@@ -10,13 +10,13 @@ db = SQLite3::Database.new('kittens.db')
 #create table
 # ruby string
 create_table = <<-SQL
-  create table if not exists kittens (
-    id integer priary key,
+  create table if not exists kitten (
+    id integer primary key,
     name varchar(255),
     age int
   );
 SQL
 
 db.execute(create_table)
-db.execute("insert into kittens (name,age) values ('mr mittens',2)")
+db.execute("insert into kitten (name,age) values ('mr mittens',2)")
 #create a test
