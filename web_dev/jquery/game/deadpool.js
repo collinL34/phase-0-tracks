c495581ad91ba2a78b87a2fcf6d39b668b6185ac
collinL34 <collinlshelby@gmail.com>
2016-09-28 13:56:11 -0700

    js file created

---
+++ web_dev/jquery/game/deadpool.js
@@ -0,0 +1,22 @@
+
+var userConfirm = confirm("Would you like to paly a game?");
+
+switch(userConfirm) {
+  case true:
+    if (userConfirm === true) var user = confirm('Okay the point of this game is for me to play around with jquery and yeah so if you dont like it well.....sorry?');
+      if (userConfirm === true) {
+        var user = confirm('Okay the point of this game is for me to play around with jquery and yeah so if you dont like it well.....sorry?');
+        var user = prompt('Okay time for some simple questions. What is 2 + 2?');
+        if (user === '4') {
+          $('html').css('background',"url('http://www.sideshowtoy.com/wp-content/uploads/2016/01/marvel-deadpool-sixth-scale-hot-toys-feature-902628.jpg')"),
+          $('html').css('background-size','cover');
+          var user = prompt("Oh well someone graduated from college. Now here's a tough one, what color is the sky?");
+          if (user === 'blue') {
+            $('html').css('background',"url('image.jpg')",'no-repeat','center','center', 'fixed'), $('html').css('background-size','cover');
+            var user = alert('wow you did it great job now to play again click the button!');
+          }
+        }
+      }
+  default:
+    alert('Okay be that way.');
+}
